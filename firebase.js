ef303dee939f68e5646865329c24f4c2796ae411
minjeong46 <eyou46@naver.com>
2025-08-04 14:19:48 +0900

    feat: ReaaltimeDB 에 캐릭터 위치 반영
    DB 에 위치 실시간 반영

--- firebase.js
+++ firebase.js
@@ -4,14 +4,13 @@ import { getDatabase } from "firebase/database";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "",
+    // ...
+    // The value of `databaseURL` depends on the location of the database
+    databaseURL: "https://gather-town-4353a-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+export const database = getDatabase(app);
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
+import { ref, onValue, set } from "firebase/database";
 import "./App.css";
+import { database } from "../firebase";
 
 function App() {
     const [position, setPosition] = useState({ x: 100, y: 100 });
 
+    function movePosition(x, y) {
+        const getRef = ref(database, "/");
+        set(getRef, {
+            x: x,
+            y: y,
+        }).catch((err)=>{
+            console.log(err);
+        });
+    }
+
     useEffect(() => {
         const handleKeyDown = (e) => {
-            console.log(e.key);
             setPosition((prev) => {
                 const next = { ...prev };
                 if (e.key === "ArrowUp") next.y -= 10;
                 else if (e.key === "ArrowDown") next.y += 10;
                 else if (e.key === "ArrowLeft") next.x -= 10;
                 else if (e.key === "ArrowRight") next.x += 10;
-                return next
+                movePosition(next.x, next.y);
+                return next;
             });
         };
 
@@ -21,13 +33,24 @@ function App() {
         return () => document.removeEventListener("keydown", handleKeyDown);
     }, []);
 
+    useEffect(() => {
+        const getRef = ref(database, "/");
+        onValue(getRef, (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                setPosition(data);
+            }
+        });
+    }, []);
+
     return (
-     position && (<div className="container">
-            <div
-                className="character"
-                style={{ left: position.x + "px", top: position.y + "px" }}
-            ></div>
-        </div>
+        position && (
+            <div className="container">
+                <div
+                    className="character"
+                    style={{ left: position.x + "px", top: position.y + "px", }}
+                ></div>
+            </div>
         )
 
         // 이미지는 <a href="/kr">Freeimages.com</a>에서 가져왔습니다.
